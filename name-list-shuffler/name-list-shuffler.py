7e6da0d849530744ec444350102e5a2413c3449c
DaKheera47 <shaheergoodboy@gmail.com>
2023-11-13 09:29:55 +0000

    Improving file structure

--- name-list-shuffler/name-list-shuffler.py
+++ name-list-shuffler/name-list-shuffler.py
@@ -3,6 +3,7 @@
 
 import random
 
+MAX_NAMES = 4000
 
 # loading files into lists
 with open("First Names.txt") as f:
@@ -20,24 +21,25 @@ first_names = [name.strip() for name in first_names]
 last_names = [name.strip() for name in last_names]
 
 # clip to 4000 in both lists
-first_names = first_names[:4000]
-last_names = last_names[:4000]
+first_names = first_names[:MAX_NAMES]
+last_names = last_names[:MAX_NAMES]
 
 # print lengths of lists
 print("First Names: ", len(first_names))
 print("Last Names: ", len(last_names))
 
 full_names = []
-
 # create full names by randomly combining first and last names
-while len(full_names) < 4000:
+while len(full_names) < MAX_NAMES:
     first_name = random.choice(first_names)
     last_name = random.choice(last_names)
+
     name = first_name + " " + last_name
 
     full_names.append(name)
 
 # get the longest name and print number of characters
+# key is the function that is used to determine the max, so in our case we want to use the length of the string
 longest_name = max(full_names, key=len)
 print("Longest Name: ", longest_name)
 print("Length: ", len(longest_name))
