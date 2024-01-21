dabd15dd7cac4c84a9b75cfd06d83228f1df5f97
DaKheera47 <shaheergoodboy@gmail.com>
2024-01-21 11:53:48 +0000

    checking for duplicates in random number generation

--- rng-data-sorter/utils.py
+++ rng-data-sorter/utils.py
@@ -8,38 +8,45 @@ def generate_random_number(numDigits, elements):
     # and repeat this process elements times
 
     # return a list of random numbers
-    randomNumbers = []
+    output_arr = []
     # 10 ** 3 = 1000
-    minNum = 10 ** (numDigits - 1)
+    min_num = 10 ** (numDigits - 1)
     # 10 ** 4 = 10000 - 1 = 9999
-    maxNum = (10**numDigits) - 1
+    max_num = (10**numDigits) - 1
 
     for _ in range(elements):
-        randomNumber = random.randint(minNum, maxNum)
-        randomNumbers.append(randomNumber)
+        rand_number = random.randint(min_num, max_num)
 
-    return randomNumbers
+        # check if the random number is already in the list
+        # if it is, generate another random number
+        # repeat this process until a unique random number is generated
+        while rand_number in output_arr:
+            rand_number = random.randint(min_num, max_num)
+
+        output_arr.append(rand_number)
+
+    return output_arr
 
 
-def time_array_sort(array, sortType):
+def time_array_sort(array, sort_type):
     # using time.perf_counter() instead of time.time()
     # as it is more accurate
 
     # start timer
     start = time.perf_counter()
 
-    if sortType == "selection":
+    if sort_type == "selection":
         sorted_arr, comparisons = selection_sort(array)
-    elif sortType == "merge":
+    elif sort_type == "merge":
         sorted_arr, comparisons = merge_sort(array)
-    elif sortType == "quick":
+    elif sort_type == "quick":
         sorted_arr, comparisons = quick_sort(array)
 
     # end timer
     end = time.perf_counter()
     # calculate time taken to sort array
-    timeTaken = end - start
+    time_taken = end - start
     # returning time in milliseconds
-    timeTaken = timeTaken * 1000
+    time_taken = time_taken * 1000
 
-    return sorted_arr, comparisons, timeTaken
+    return sorted_arr, comparisons, time_taken
